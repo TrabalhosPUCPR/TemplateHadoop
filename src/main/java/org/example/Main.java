c8e9afc85f39bb2a160112f663b3da78cd5d6b29
KnightLeo <knightleo602@gmail.com>
2023-03-30 22:14:28 -0300

    Fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
-public class App {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello world!");
     }
 }
